f1760da32948f764d4dc0812f8c27f5a9a2959c9
José Trejo <joseatrejos>
2019-10-07 14:13:21 -0700

    Initial Commit

---
+++ iOSProyectoParcial2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSProyectoParcial2
+//
+//  Created by Alumno on 10/7/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
